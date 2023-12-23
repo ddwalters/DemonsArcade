4bfaeb059a2b4f2c941ed09cead4a603d0e5713a
Dylan_W <ddwalters789@gmail.com>
2023-12-23 18:11:05 -0500

    Code Fix

--- Assets/Scripts/Movemnet.cs
+++ Assets/Scripts/Movemnet.cs
@@ -7,8 +7,11 @@ public class Movemnet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      alkkhhsd
-            svar
-            var hi
+
+    }
+
+    void Update()
+    {
+        
     }
 }
