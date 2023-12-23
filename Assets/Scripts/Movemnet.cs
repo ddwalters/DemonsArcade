e72689b6f019eca90decf384311aa18cae5b7b25
Dylan_W <ddwalters789@gmail.com>
2023-12-23 18:08:18 -0500

    Commit

---
+++ Assets/Scripts/Movemnet.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movemnet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      alkkhhsd
+            svar
+            var hi
+    }
+}
