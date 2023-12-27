04fee9f62e4e1b577b29708ff280b168fc07196f
Dylan_W <ddwalters789@gmail.com>
2023-12-27 17:35:00 -0500

    Simple player movement start

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,12 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movemnet : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     void Update()
