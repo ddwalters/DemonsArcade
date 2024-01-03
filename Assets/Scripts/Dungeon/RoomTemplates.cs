5165c81efc928036fd1eb8ac27ffcf0477ca1825
TheDemonDonut <bgardner127@gmail.com>
2024-01-02 20:10:18 -0800

    room template script change

--- Assets/Scripts/Dungeon/RoomTemplates.cs
+++ Assets/Scripts/Dungeon/RoomTemplates.cs
@@ -18,6 +18,15 @@ public class RoomTemplates : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Iterate through the list in reverse to safely remove elements
+        for (int i = Rooms.Count - 1; i >= 0; i--)
+        {
+            // Check if the element is null or has been destroyed
+            if (Rooms[i] == null || Rooms[i].Equals(null))
+            {
+                // Remove the empty game object from the list
+                Rooms.RemoveAt(i);
+            }
+        }
     }
 }
