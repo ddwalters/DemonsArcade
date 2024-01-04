21971bfca5477f3fac79e41f0ab1742069fd67c3
Dylan_W <ddwalters789@gmail.com>
2024-01-03 20:20:03 -0500

    Touch and ray interaction

--- Assets/Scripts/Dungeon/Chest.cs
+++ Assets/Scripts/Dungeon/Chest.cs
@@ -4,12 +4,34 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour, IInteractable
 {
-    [SerializeField] private string _prompt;
-
+    [SerializeField] string _prompt;
     public string InteractionPrompt => _prompt;
+
+    GameObject lootChestUI;
+    GameObject backpackUI;
+
+    void Start()
+    {
+        lootChestUI = GameObject.FindGameObjectWithTag("LootView");
+        backpackUI = GameObject.FindGameObjectWithTag("InventoryView");
+
+        backpackUI.SetActive(false);
+        lootChestUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            backpackUI.SetActive(false);
+            lootChestUI.SetActive(false);
+        }
+    }
+
     public bool Interact(Interactor interactor)
     {
-        Debug.Log("Opening chest");
+        backpackUI.SetActive(true);
+        lootChestUI.SetActive(true);
         return true;
-    }
+    }    
 }
--- Assets/Scripts/Game Manager/Interactor.cs
+++ Assets/Scripts/Game Manager/Interactor.cs
@@ -5,28 +5,39 @@ using UnityEngine.InputSystem;
 
 public class Interactor : MonoBehaviour
 {
+    [Header("Touch Interactor")]
     [SerializeField] private Transform _interactionPoint;
     [SerializeField] private float _interactionPointRadius = 0.5f;
     [SerializeField] private LayerMask _interactableMask;
+    [SerializeField] private GameObject interactableMessageObject;
     [SerializeField] private InteractionPromptUI _interactionPromptUI;
-    
+
     private readonly Collider[] _colliders = new Collider[3];
     [SerializeField] private int _numFound;
 
+    [Header("Ray Interactor")]
+    [SerializeField] private Transform _rayInteractorSource;
+    [SerializeField] private float _rayInteractRange;
+
     private IInteractable _interactable;
+
+    void Start()
+    {
+        _interactionPromptUI = interactableMessageObject.GetComponent<InteractionPromptUI>();
+    }
 
     void Update()
     {
+        // touch interaction  -- for opening chest, doors, ect.
         _numFound = Physics.OverlapSphereNonAlloc(_interactionPoint.position, _interactionPointRadius, _colliders, _interactableMask);
-        
+
         if (_numFound > 0)
         {
             _interactable = _colliders[0].GetComponent<IInteractable>();
-        
+            _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
+
             if (_interactable != null && Input.GetKeyUp(KeyCode.E))
             {
-                _interactionPromptUI.SetUp(_interactable.InteractionPrompt);
-
                 if (Input.GetKeyUp(KeyCode.E))
                     _interactable.Interact(this);
             }
@@ -35,8 +46,17 @@ public class Interactor : MonoBehaviour
         {
             if (_interactable != null)
                 _interactable = null;
-            
+
             _interactionPromptUI.Close();
+        }
+
+        //raycast interaction -- for collecting loot on da floor
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            Ray ray = new Ray(_rayInteractorSource.position, _rayInteractorSource.forward);
+            if (Physics.Raycast(ray, out RaycastHit hitInfo, _rayInteractRange))
+                if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactableObj))
+                    interactableObj.Interact(this);
         }
     }
 
