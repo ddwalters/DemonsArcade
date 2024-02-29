462683d9c6e2a66f7d5e8600a756217d5edc381d
Dylan-Walters-Student <20100706@zpsstudents.org>
2024-02-29 14:45:36 -0500

    Player stats file start

---
+++ Assets/StatsSystem/PlayerStats.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    int currentLvl;
+    int currentXP;
+    int nextLevelXP;
+    // xp slider (should have tag)
+    // lvl text
+
+    int maxHealth;
+    int currentHealth;
+    // health text
+    // hud health icon (should have tag)
+
+    int maxStamina;
+    int currentStamina;
+    // stamina text
+    // hud stamina slider (should have tag)
+
+    int maxMana;
+    int currentMana;
+    // mana text
+    // hud mana slider (should have tag)
+
+    private void Start()
+    {
+        // get xp slider by tag
+        // get health slider by tag
+        // get stamina slider by tag
+        // get mana slider by tag
+    }
+}
