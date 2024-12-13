0291b01676435185a1bd3e1297d9563b3235b0e0
TheDemonDonut <bgardner127@gmail.com>
2024-12-12 20:35:36 -0800

    open player grid

--- Assets/Scripts/Inventory scripts/Grid.cs
+++ Assets/Scripts/Inventory scripts/Grid.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 enum GridType
 {
@@ -15,22 +16,51 @@ public class Grid : MonoBehaviour
 
     private int gridId;
     private List<((int x, int y), int itemData)> items;
+    private List<GameObject> itemSlotPrefabs = new List<GameObject>();
 
     [SerializeField] GridType gridType;
 
+    [SerializeField] GameObject slotPrefab; //@DW this needs to become a scriptable object
+
+    private GridLayoutGroup playerLayoutGroup;
+
+    public bool yes = false;
+
+    public bool no = false;
+
     private void Awake()
     {
-        gridReader = FindAnyObjectByType<GridManager>();
+        //gridReader = FindAnyObjectByType<GridManager>();
     }
 
     private void OnEnable()
     {
-        gridId = gridReader.GetNewGridId();
+        //gridId = gridReader.GetNewGridId();
     }
 
     void Start()
     {
-        items = gridReader.GetGridItems(gridId);
+        //items = gridReader.GetGridItems(gridId);
+
+        if (gridType == GridType.Player)
+        {
+            playerLayoutGroup = GetComponentInChildren<GridLayoutGroup>();
+        }
+    }
+
+    private void Update()
+    {
+        if (yes)
+        {
+            OpenPlayer();
+            yes = false;
+        }
+
+        if (no)
+        {
+            ClosePlayer();
+            no = false;
+        }
     }
 
     #region Actions
@@ -76,12 +106,16 @@ public class Grid : MonoBehaviour
     #region Player Grid __ NOT IMPLEMENTED
     void OpenPlayer()
     {
-
+        for (int i = 1; i <= 25; i++)
+        {
+            GameObject go = Instantiate(slotPrefab, playerLayoutGroup.transform);
+            itemSlotPrefabs.Add(go);
+        }
     }
 
     void ClosePlayer()
     {
-
+        itemSlotPrefabs.Clear();
     }
     #endregion
 
